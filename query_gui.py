cb1eeb16b34de09cb567825c08feca8cd4c53784
nwolfe195 <nicholaswolfe195@gmail.com>
2022-05-09 20:00:20 -0400

    Added correlation print out to terminal

--- query_gui.py
+++ query_gui.py
@@ -3,9 +3,10 @@ from pytrends_processing import *
 import matplotlib.pyplot as plt
 from tkcalendar import DateEntry
 from datetime import datetime
+import numpy as np
 
 
-class MainApplication(Frame):
+class QueryGui(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
@@ -21,7 +22,21 @@ class MainApplication(Frame):
         Label(text='End Date').grid(row=3, column=0)
         self.end_date = DateEntry()
         self.end_date.grid(row=3, column=1)
-        Button(text='Search Trend!', command=self.search).grid(row=4, column=0, columnspan=2)
+        Button(text='Search Trend', command=self.trend_graph).grid(row=4, column=0, columnspan=2)
+        Button(text='Get Correlation', command=self.correlation).grid(row=5, column=0, columnspan=2)
+
+    def trend_graph(self):
+        data = self.search()
+        data.set_index('date').plot(kind='line')
+        plt.show()
+
+    def correlation(self):
+        data = self.search()
+        corr = data.corr()
+        stack = corr.stack()
+        no_self_pair = stack[stack.index.get_level_values(0) != stack.index.get_level_values(1)]
+        no_self_or_dup_pair = no_self_pair[no_self_pair.index.get_level_values(0) < no_self_pair.index.get_level_values(1)]
+        no_self_or_dup_pair_df = no_self_or_dup_pair.to_frame()
 
     def search(self):
         keywords_input = self.keywords.get("1.0", "end")
@@ -30,17 +45,12 @@ class MainApplication(Frame):
         end_date = str(datetime.strptime(self.end_date.get(), '%m/%d/%y').date())
         countries = ['US']
         keyword_search_results = TrendsData(keywords, start_date, end_date, countries).get_trends()
-        self.graph(keyword_search_results)
-
-    def graph(self, data):
-        data.set_index('date').plot(kind='line')
-        print(data)
-        plt.show()
+        return keyword_search_results
 
 
 if __name__ == "__main__":
     root = Tk()
     root.title('Google Trend Search')
     root.geometry('500x250')
-    MainApplication(root)
+    QueryGui(root)
     root.mainloop()
